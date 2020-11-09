2f1d19ccfc3812ff3a39970914b3f43ea914fd89
Radek Simko <radek.simko@gmail.com>
2020-11-09 16:21:53 +0000

    schema/attribute: introduce explicit IsOptional field
    This is to differentiate between fields which are
    computed-only (IsComputed: true, IsOptional: false)
    and computed + optional (IsComputed: true, IsOptional: true).
    
    Technically we could assume IsOptional: true when IsComputed: false,
    but that would likely make a future serialization story more complex
    and introduce potential confusion for callers where they may just read
    raw struct field, instead of going through some functions
    which set the defaults.

--- schema/attribute_schema.go
+++ schema/attribute_schema.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"errors"
+
 	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -9,8 +11,9 @@ import (
 type AttributeSchema struct {
 	Description  lang.MarkupContent
 	IsRequired   bool
+	IsOptional   bool
 	IsDeprecated bool
-	IsReadOnly   bool
+	IsComputed   bool
 
 	ValueType  cty.Type
 	ValueTypes ValueTypes
@@ -32,4 +35,27 @@ func (vt ValueTypes) FriendlyNames() []string {
 
 func (*AttributeSchema) isSchemaImpl() schemaImplSigil {
 	return schemaImplSigil{}
+}
+
+func (as *AttributeSchema) Validate() error {
+	if len(as.ValueTypes) == 0 && as.ValueType == cty.NilType {
+		return errors.New("one of ValueType or ValueTypes must be specified")
+	}
+	if len(as.ValueTypes) > 0 && as.ValueType != cty.NilType {
+		return errors.New("ValueType or ValueTypes must be specified, not both")
+	}
+
+	if as.IsOptional && as.IsRequired {
+		return errors.New("IsOptional or IsRequired must be set, not both")
+	}
+
+	if as.IsRequired && as.IsComputed {
+		return errors.New("cannot be both IsRequired and IsComputed")
+	}
+
+	if !as.IsRequired && !as.IsOptional && !as.IsComputed {
+		return errors.New("one of IsRequired, IsOptional, or IsComputed must be set")
+	}
+
+	return nil
 }
