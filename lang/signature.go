a758be435fda1b7368c2cbd3af134675ff34c521
Daniel Banck <daniel@dbanck.de>
2022-09-23 16:47:48 +0200

    Add new function signature types

---
+++ lang/signature.go
@@ -0,0 +1,24 @@
+package lang
+
+type FunctionSignature struct {
+	Name string
+
+	// Description is an optional human-readable description
+	// of the function.
+	Description MarkupContent
+
+	// Parameters is an ordered list of the function's parameters.
+	Parameters []FunctionParameter
+
+	// ActiveParameter is an index marking the parameter a user is currently
+	// editing. It should lie inside the range of Parameters.
+	ActiveParameter uint32
+}
+
+type FunctionParameter struct {
+	Name string
+
+	// Description is an optional human-readable description
+	// of the parameter.
+	Description MarkupContent
+}
