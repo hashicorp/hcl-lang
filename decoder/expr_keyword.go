e583af59d77df2ce747bfa0d96381a850faf73ed
Radek Simko <radek.simko@gmail.com>
2023-01-24 13:48:55 +0200

    decoder: Implement completion for Keyword

--- decoder/expr_keyword.go
+++ decoder/expr_keyword.go
@@ -13,11 +13,6 @@ type Keyword struct {
 	cons schema.Keyword
 }
 
-func (kw Keyword) CompletionAtPos(ctx context.Context, pos hcl.Pos) []lang.Candidate {
-	// TODO
-	return nil
-}
-
 func (kw Keyword) HoverAtPos(ctx context.Context, pos hcl.Pos) *lang.HoverData {
 	// TODO
 	return nil
