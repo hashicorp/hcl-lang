b355e443aba466c98001409521757556a2a035d8
Radek Simko <radek.simko@gmail.com>
2023-01-25 13:18:26 +0200

    decoder: Implement semantic tokens for List

--- decoder/expr_list.go
+++ decoder/expr_list.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"context"
 
-	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
@@ -13,11 +12,6 @@ type List struct {
 	expr    hcl.Expression
 	cons    schema.List
 	pathCtx *PathContext
-}
-
-func (l List) SemanticTokens(ctx context.Context) []lang.SemanticToken {
-	// TODO
-	return nil
 }
 
 func (l List) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
