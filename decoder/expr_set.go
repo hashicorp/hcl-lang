0bfc811f87543dcbbaf7309b16f36826ec04d9c1
Radek Simko <radek.simko@gmail.com>
2023-01-25 13:39:00 +0200

    decoder: Implement semantic tokens for Set

--- decoder/expr_set.go
+++ decoder/expr_set.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"context"
 
-	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
@@ -13,11 +12,6 @@ type Set struct {
 	expr    hcl.Expression
 	cons    schema.Set
 	pathCtx *PathContext
-}
-
-func (s Set) SemanticTokens(ctx context.Context) []lang.SemanticToken {
-	// TODO
-	return nil
 }
 
 func (s Set) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
---
+++ decoder/expr_set_semtok.go
@@ -0,0 +1,28 @@
+package decoder
+
+import (
+	"context"
+
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+)
+
+func (set Set) SemanticTokens(ctx context.Context) []lang.SemanticToken {
+	eType, ok := set.expr.(*hclsyntax.TupleConsExpr)
+	if !ok {
+		return []lang.SemanticToken{}
+	}
+
+	if len(eType.Exprs) == 0 || set.cons.Elem == nil {
+		return []lang.SemanticToken{}
+	}
+
+	tokens := make([]lang.SemanticToken, 0)
+
+	for _, elemExpr := range eType.Exprs {
+		expr := newExpression(set.pathCtx, elemExpr, set.cons.Elem)
+		tokens = append(tokens, expr.SemanticTokens(ctx)...)
+	}
+
+	return tokens
+}
