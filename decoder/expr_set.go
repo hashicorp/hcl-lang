fb96f2b262a63dac1e84b364d8b84bc7e07d5cc3
Radek Simko <radek.simko@gmail.com>
2023-01-25 13:37:41 +0200

    decoder: Implement hover for Set

--- decoder/expr_set.go
+++ decoder/expr_set.go
@@ -15,11 +15,6 @@ type Set struct {
 	pathCtx *PathContext
 }
 
-func (s Set) HoverAtPos(ctx context.Context, pos hcl.Pos) *lang.HoverData {
-	// TODO
-	return nil
-}
-
 func (s Set) SemanticTokens(ctx context.Context) []lang.SemanticToken {
 	// TODO
 	return nil
