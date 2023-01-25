6161d904dadf8dea28fd6cc4a02565616cb380ce
Radek Simko <radek.simko@gmail.com>
2023-01-25 13:40:47 +0200

    decoder: Implement reference origins for Set

--- decoder/expr_set.go
+++ decoder/expr_set.go
@@ -1,9 +1,6 @@
 package decoder
 
 import (
-	"context"
-
-	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
 )
@@ -12,9 +9,4 @@ type Set struct {
 	expr    hcl.Expression
 	cons    schema.Set
 	pathCtx *PathContext
-}
-
-func (s Set) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
-	// TODO
-	return nil
 }
