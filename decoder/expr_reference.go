d441290450deab2f4ffe652146fbe913039cfaf4
Radek Simko <radek.simko@gmail.com>
2023-01-28 16:22:48 +0200

    decoder: Implement reference origins for Reference

--- decoder/expr_reference.go
+++ decoder/expr_reference.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"context"
 
-	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
@@ -13,11 +12,6 @@ type Reference struct {
 	expr    hcl.Expression
 	cons    schema.Reference
 	pathCtx *PathContext
-}
-
-func (ref Reference) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
-	// TODO
-	return nil
 }
 
 func (ref Reference) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) reference.Targets {
---
+++ decoder/expr_reference_ref_origins.go
@@ -0,0 +1,84 @@
+package decoder
+
+import (
+	"context"
+
+	"github.com/hashicorp/hcl-lang/reference"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/json"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func (ref Reference) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
+	// deal with native HCL syntax first
+	te, ok := ref.expr.(*hclsyntax.ScopeTraversalExpr)
+	if ok {
+		origin, ok := reference.TraversalToLocalOrigin(te.Traversal, ref.cons, allowSelfRefs)
+		if ok {
+			return reference.Origins{origin}
+		}
+	}
+
+	if json.IsJSONExpression(ref.expr) {
+		// Given the limited AST/API access to JSON we can only
+		// guess whether the expression has exactly a single traversal
+		vars := ref.expr.Variables()
+		if len(vars) == 1 {
+			tRange := vars[0].SourceRange()
+			expectedExprRange := hcl.Range{
+				Filename: tRange.Filename,
+				Start: hcl.Pos{
+					Line: tRange.Start.Line,
+					// account for "${
+					Column: tRange.Start.Column - 3,
+					Byte:   tRange.Start.Byte - 3,
+				},
+				End: hcl.Pos{
+					Line: tRange.End.Line,
+					// account for }"
+					Column: tRange.End.Column + 2,
+					Byte:   tRange.End.Byte + 2,
+				},
+			}
+
+			if rangesEqual(expectedExprRange, ref.expr.Range()) {
+				origin, ok := reference.TraversalToLocalOrigin(vars[0], ref.cons, allowSelfRefs)
+				if ok {
+					return reference.Origins{origin}
+				}
+			}
+		}
+
+		// Account for "legacy" string syntax which is still
+		// in use by Terraform to date in this context.
+		val, diags := ref.expr.Value(nil)
+		if diags.HasErrors() {
+			return reference.Origins{}
+		}
+		if val.Type() != cty.String {
+			return reference.Origins{}
+		}
+		startPos := hcl.Pos{
+			Line: ref.expr.Range().Start.Line,
+			// Account for the leading double quote
+			Column: ref.expr.Range().Start.Column + 1,
+			Byte:   ref.expr.Range().Start.Byte + 1,
+		}
+
+		traversal, diags := hclsyntax.ParseTraversalAbs([]byte(val.AsString()), ref.expr.Range().Filename, startPos)
+		if diags.HasErrors() {
+			return reference.Origins{}
+		}
+		origin, ok := reference.TraversalToLocalOrigin(traversal, ref.cons, allowSelfRefs)
+		if ok {
+			return reference.Origins{origin}
+		}
+	}
+
+	return reference.Origins{}
+}
+
+func rangesEqual(first, second hcl.Range) bool {
+	return posEqual(first.Start, second.Start) && posEqual(first.End, second.End)
+}
