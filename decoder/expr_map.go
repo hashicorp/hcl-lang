6dfabb88d846d90b0322a901d26921915574af8a
Radek Simko <radek.simko@gmail.com>
2023-01-30 15:10:27 +0000

    decoder: Implement reference targets for Map

--- decoder/expr_map.go
+++ decoder/expr_map.go
@@ -1,9 +1,6 @@
 package decoder
 
 import (
-	"context"
-
-	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
 )
@@ -12,9 +9,4 @@ type Map struct {
 	expr    hcl.Expression
 	cons    schema.Map
 	pathCtx *PathContext
-}
-
-func (m Map) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) reference.Targets {
-	// TODO
-	return nil
 }
