ec0f8b207c252fd00e2413909ce37c6af86519ee
Radek Simko <radek.simko@gmail.com>
2023-02-21 10:05:44 +0000

    decoder: Implement semantic tokens for Object

--- decoder/expr_object.go
+++ decoder/expr_object.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"context"
 
-	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
@@ -13,11 +12,6 @@ type Object struct {
 	expr    hcl.Expression
 	cons    schema.Object
 	pathCtx *PathContext
-}
-
-func (obj Object) SemanticTokens(ctx context.Context) []lang.SemanticToken {
-	// TODO
-	return nil
 }
 
 func (obj Object) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
