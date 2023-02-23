4386985b1da92396699879d6b74940d6844ace58
Radek Simko <radek.simko@gmail.com>
2023-02-23 14:19:09 +0000

    schema: Add tests for EmptyHoverData

--- schema/constraint_test.go
+++ schema/constraint_test.go
@@ -1,5 +1,14 @@
 package schema
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/zclconf/go-cty/cty"
+)
+
 var (
 	_ Constraint = AnyExpression{}
 	_ Constraint = Keyword{}
@@ -33,3 +42,152 @@ var (
 	_ TypeAwareConstraint = Set{}
 	_ TypeAwareConstraint = Tuple{}
 )
+
+func TestConstraint_EmptyHoverData(t *testing.T) {
+	testCases := []struct {
+		cons              ConstraintWithHoverData
+		expectedHoverData *HoverData
+	}{
+		{
+			LiteralType{
+				Type: cty.String,
+			},
+			&HoverData{
+				Content: lang.Markdown(`string`),
+			},
+		},
+		{
+			List{
+				Elem: LiteralType{
+					Type: cty.String,
+				},
+			},
+			&HoverData{
+				Content: lang.Markdown("list(string)"),
+			},
+		},
+		{
+			LiteralType{
+				Type: cty.List(cty.String),
+			},
+			&HoverData{
+				Content: lang.Markdown("list(string)"),
+			},
+		},
+		{
+			Set{
+				Elem: LiteralType{
+					Type: cty.String,
+				},
+			},
+			&HoverData{
+				Content: lang.Markdown("set(string)"),
+			},
+		},
+		{
+			LiteralType{
+				Type: cty.Set(cty.String),
+			},
+			&HoverData{
+				Content: lang.Markdown("set(string)"),
+			},
+		},
+		{
+			LiteralType{
+				Type: cty.Object(map[string]cty.Type{
+					"foo": cty.String,
+					"bar": cty.Number,
+					"baz": cty.List(cty.String),
+				}),
+			},
+			&HoverData{
+				Content: lang.Markdown("```" + `
+{
+  bar = number
+  baz = list(string)
+  foo = string
+}
+` + "```\n"),
+			},
+		},
+		{
+			LiteralType{
+				Type: cty.Object(map[string]cty.Type{
+					"foo": cty.String,
+					"bar": cty.Number,
+					"baz": cty.Object(map[string]cty.Type{
+						"foo": cty.String,
+						"bar": cty.Number,
+					}),
+				}),
+			},
+			&HoverData{
+				Content: lang.Markdown("```" + `
+{
+  bar = number
+  baz = {
+    bar = number
+    foo = string
+  }
+  foo = string
+}
+` + "```\n"),
+			},
+		},
+		// negative tests
+		{
+			List{
+				Elem: Keyword{
+					Keyword: "kw",
+				},
+			},
+			nil,
+		},
+		{
+			Set{
+				Elem: Keyword{
+					Keyword: "kw",
+				},
+			},
+			nil,
+		},
+		{
+			Tuple{
+				Elems: []Constraint{
+					Keyword{
+						Keyword: "kw",
+					},
+				},
+			},
+			nil,
+		},
+		{
+			Map{
+				Elem: Keyword{
+					Keyword: "kw",
+				},
+			},
+			nil,
+		},
+		{
+			Object{
+				Attributes: map[string]*AttributeSchema{
+					"foo": {
+						Constraint: Keyword{
+							Keyword: "kw",
+						},
+					},
+				},
+			},
+			nil,
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			hoverData := tc.cons.EmptyHoverData(0)
+			if diff := cmp.Diff(tc.expectedHoverData, hoverData); diff != "" {
+				t.Fatalf("unexpected hover data: %s", diff)
+			}
+		})
+	}
+}
