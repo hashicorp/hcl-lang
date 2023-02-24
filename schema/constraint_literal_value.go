419de4905610d74fbe51b32fffaec958cc445e83
Radek Simko <radek.simko@gmail.com>
2023-02-24 12:29:53 +0000

    schema: Implement EmptyCompletionData for LiteralValue

--- schema/constraint_literal_value.go
+++ schema/constraint_literal_value.go
@@ -39,8 +39,179 @@ func (lv LiteralValue) Copy() Constraint {
 }
 
 func (lv LiteralValue) EmptyCompletionData(nextPlaceholder int, nestingLevel int) CompletionData {
-	// TODO
-	return CompletionData{}
+	if lv.Value.Type().IsPrimitiveType() {
+		var value string
+		switch lv.Value.Type() {
+		case cty.Bool:
+			value = fmt.Sprintf("%t", lv.Value.True())
+		case cty.String:
+			if strings.ContainsAny(lv.Value.AsString(), "\n") && nestingLevel == 0 {
+				// avoid double newline
+				strValue := strings.TrimSuffix(lv.Value.AsString(), "\n")
+				value = fmt.Sprintf("<<<STRING\n%s\nSTRING", strValue)
+				if nestingLevel == 0 {
+					value += "\n"
+				}
+			} else {
+				value = fmt.Sprintf("%q", lv.Value.AsString())
+			}
+		case cty.Number:
+			value = formatNumberVal(lv.Value)
+		}
+
+		return CompletionData{
+			NewText:         value,
+			Snippet:         value,
+			LastPlaceholder: nextPlaceholder,
+		}
+	}
+	if lv.Value.Type().IsListType() {
+		vals := lv.Value.AsValueSlice()
+		elemNewText := make([]string, len(vals))
+		elemSnippets := make([]string, len(vals))
+		lastPlaceholder := nextPlaceholder
+
+		for i, val := range vals {
+			c := LiteralValue{
+				Value: val,
+			}
+			cData := c.EmptyCompletionData(lastPlaceholder, nestingLevel)
+			if cData.NewText == "" || cData.Snippet == "" {
+				return CompletionData{
+					LastPlaceholder: lastPlaceholder,
+				}
+			}
+			elemNewText[i] = cData.NewText
+			elemSnippets[i] = cData.Snippet
+			lastPlaceholder = cData.LastPlaceholder
+		}
+
+		return CompletionData{
+			// TODO: consider wrapping this in tolist()
+			NewText:         fmt.Sprintf("[%s]", strings.Join(elemNewText, ", ")),
+			Snippet:         fmt.Sprintf("[%s]", strings.Join(elemSnippets, ", ")),
+			LastPlaceholder: lastPlaceholder,
+		}
+	}
+	if lv.Value.Type().IsSetType() {
+		vals := lv.Value.AsValueSlice()
+		elemNewText := make([]string, len(vals))
+		elemSnippets := make([]string, len(vals))
+		lastPlaceholder := nextPlaceholder
+
+		for i, val := range vals {
+			c := LiteralValue{
+				Value: val,
+			}
+			cData := c.EmptyCompletionData(lastPlaceholder, nestingLevel)
+			if cData.NewText == "" || cData.Snippet == "" {
+				return CompletionData{
+					LastPlaceholder: lastPlaceholder,
+				}
+			}
+			elemNewText[i] = cData.NewText
+			elemSnippets[i] = cData.Snippet
+			lastPlaceholder = cData.LastPlaceholder
+		}
+
+		return CompletionData{
+			// TODO: consider wrapping this in toset()
+			NewText:         fmt.Sprintf("[%s]", strings.Join(elemNewText, ", ")),
+			Snippet:         fmt.Sprintf("[%s]", strings.Join(elemSnippets, ", ")),
+			LastPlaceholder: lastPlaceholder,
+		}
+	}
+	if lv.Value.Type().IsTupleType() {
+		vals := lv.Value.AsValueSlice()
+		elemNewText := make([]string, len(vals))
+		elemSnippets := make([]string, len(vals))
+		lastPlaceholder := nextPlaceholder
+
+		for i, val := range vals {
+			c := LiteralValue{
+				Value: val,
+			}
+			cData := c.EmptyCompletionData(lastPlaceholder, nestingLevel)
+			if cData.NewText == "" || cData.Snippet == "" {
+				return CompletionData{
+					LastPlaceholder: lastPlaceholder,
+				}
+			}
+			elemNewText[i] = cData.NewText
+			elemSnippets[i] = cData.Snippet
+			lastPlaceholder = cData.LastPlaceholder
+		}
+
+		return CompletionData{
+			NewText:         fmt.Sprintf("[%s]", strings.Join(elemNewText, ", ")),
+			Snippet:         fmt.Sprintf("[%s]", strings.Join(elemSnippets, ", ")),
+			LastPlaceholder: lastPlaceholder,
+		}
+	}
+	if lv.Value.Type().IsMapType() {
+		valueMap := lv.Value.AsValueMap()
+
+		attrNames := sortedValueMap(valueMap)
+
+		// TODO: consider wrapping in tomap()
+		newText, snippet := "{\n", "{\n"
+		lastPlaceholder := nextPlaceholder
+		for _, name := range attrNames {
+			val := valueMap[name]
+
+			cons := LiteralValue{
+				Value: val,
+			}
+
+			cData := cons.EmptyCompletionData(lastPlaceholder, nestingLevel+1)
+			if cData.NewText == "" || cData.Snippet == "" {
+				return CompletionData{
+					LastPlaceholder: lastPlaceholder,
+				}
+			}
+
+			newText += fmt.Sprintf("%s%q = %s\n",
+				strings.Repeat("  ", nestingLevel+1),
+				name, cData.NewText)
+			snippet += fmt.Sprintf("%s%q = %s\n",
+				strings.Repeat("  ", nestingLevel+1),
+				name, cData.Snippet)
+			lastPlaceholder = cData.LastPlaceholder
+		}
+		newText += fmt.Sprintf("%s}", strings.Repeat("  ", nestingLevel))
+		snippet += fmt.Sprintf("%s}", strings.Repeat("  ", nestingLevel))
+
+		return CompletionData{
+			NewText:         newText,
+			Snippet:         snippet,
+			LastPlaceholder: lastPlaceholder,
+		}
+	}
+	if lv.Value.Type().IsObjectType() {
+		valueMap := lv.Value.AsValueMap()
+		attrs := make(ObjectAttributes, 0)
+		for name, attrValue := range valueMap {
+			aSchema := &AttributeSchema{
+				Constraint: LiteralValue{
+					Value: attrValue,
+				},
+			}
+			if lv.Value.Type().AttributeOptional(name) {
+				aSchema.IsOptional = true
+			} else {
+				aSchema.IsRequired = true
+			}
+			attrs[name] = aSchema
+		}
+		cons := Object{
+			Attributes: attrs,
+		}
+		return cons.EmptyCompletionData(nextPlaceholder, nestingLevel)
+	}
+
+	return CompletionData{
+		LastPlaceholder: nextPlaceholder,
+	}
 }
 
 func (lv LiteralValue) EmptyHoverData(nestingLevel int) *HoverData {
