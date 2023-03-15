e6bcac1ae6d2d7478e683222603e132e8c44972e
Radek Simko <radek.simko@gmail.com>
2023-03-15 17:08:26 +0000

    decoder: Implement semantic tokens for LiteralType

--- decoder/expr_literal_type.go
+++ decoder/expr_literal_type.go
@@ -3,7 +3,6 @@ package decoder
 import (
 	"context"
 
-	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
@@ -14,11 +13,6 @@ type LiteralType struct {
 	cons schema.LiteralType
 
 	pathCtx *PathContext
-}
-
-func (lt LiteralType) SemanticTokens(ctx context.Context) []lang.SemanticToken {
-	// TODO
-	return nil
 }
 
 func (lt LiteralType) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
---
+++ decoder/expr_literal_type_semtok.go
@@ -0,0 +1,153 @@
+package decoder
+
+import (
+	"context"
+
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func (lt LiteralType) SemanticTokens(ctx context.Context) []lang.SemanticToken {
+	typ := lt.cons.Type
+
+	if typ == cty.DynamicPseudoType {
+		val, _ := lt.expr.Value(nil)
+		if val.IsKnown() && !val.IsNull() {
+			typ = val.Type()
+		}
+	}
+
+	// string is a special case as it's always represented like a template
+	// even if there's no templating involved
+	if typ == cty.String {
+		expr, ok := lt.expr.(*hclsyntax.TemplateExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+		if expr.IsStringLiteral() {
+			return []lang.SemanticToken{
+				{
+					Type:      lang.TokenString,
+					Modifiers: lang.SemanticTokenModifiers{},
+					Range:     expr.Range(),
+				},
+			}
+		}
+
+		// TODO: report multiline/HEREDOC notation
+
+		return []lang.SemanticToken{}
+	}
+
+	if typ.IsPrimitiveType() {
+		expr, ok := lt.expr.(*hclsyntax.LiteralValueExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		if !lt.cons.Type.Equals(expr.Val.Type()) {
+			return []lang.SemanticToken{}
+		}
+
+		if typ == cty.Bool {
+			return []lang.SemanticToken{
+				{
+					Type:      lang.TokenBool,
+					Modifiers: lang.SemanticTokenModifiers{},
+					Range:     expr.Range(),
+				},
+			}
+		}
+
+		if typ == cty.Number {
+			return []lang.SemanticToken{
+				{
+					Type:      lang.TokenNumber,
+					Modifiers: lang.SemanticTokenModifiers{},
+					Range:     expr.Range(),
+				},
+			}
+		}
+
+		return []lang.SemanticToken{}
+	}
+
+	if typ.IsListType() {
+		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.List{
+			Elem: schema.LiteralType{
+				Type: typ.ElementType(),
+			},
+		}
+
+		return newExpression(lt.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsSetType() {
+		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Set{
+			Elem: schema.LiteralType{
+				Type: typ.ElementType(),
+			},
+		}
+
+		return newExpression(lt.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsTupleType() {
+		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		elemTypes := typ.TupleElementTypes()
+		cons := schema.Tuple{
+			Elems: make([]schema.Constraint, len(elemTypes)),
+		}
+		for i, elemType := range elemTypes {
+			cons.Elems[i] = schema.LiteralType{
+				Type: elemType,
+			}
+		}
+
+		return newExpression(lt.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsMapType() {
+		expr, ok := lt.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Map{
+			Elem: schema.LiteralType{
+				Type: typ.ElementType(),
+			},
+		}
+		return newExpression(lt.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsObjectType() {
+		expr, ok := lt.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Object{
+			Attributes: ctyObjectToObjectAttributes(typ),
+		}
+		return newExpression(lt.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	return []lang.SemanticToken{}
+}
