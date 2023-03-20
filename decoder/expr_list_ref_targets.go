1984b99934571309ee074e7f83a519528b0cf036
Radek Simko <radek.simko@gmail.com>
2023-03-20 09:11:21 +0000

    decoder: Finish implementing reference targets for List

--- decoder/expr_list_ref_targets.go
+++ decoder/expr_list_ref_targets.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
+	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -19,10 +20,7 @@ func (list List) ReferenceTargets(ctx context.Context, targetCtx *TargetContext)
 		return reference.Targets{}
 	}
 
-	targets := make(reference.Targets, 0)
-
-	// TODO: collect parent target for the whole list
-	// See https://github.com/hashicorp/hcl-lang/issues/228
+	elemTargets := make(reference.Targets, 0)
 
 	for i, elemExpr := range eType.Exprs {
 		expr := newExpression(list.pathCtx, elemExpr, list.cons.Elem)
@@ -37,8 +35,49 @@ func (list List) ReferenceTargets(ctx context.Context, targetCtx *TargetContext)
 				})
 			}
 
-			targets = append(targets, e.ReferenceTargets(ctx, elemCtx)...)
+			elemTargets = append(elemTargets, e.ReferenceTargets(ctx, elemCtx)...)
 		}
+	}
+
+	targets := make(reference.Targets, 0)
+
+	if targetCtx != nil {
+		// collect target for the whole list
+
+		// type-aware
+		elemCons, ok := list.cons.Elem.(schema.TypeAwareConstraint)
+		if targetCtx.AsExprType && ok {
+			elemType, ok := elemCons.ConstraintType()
+			if ok {
+				targets = append(targets, reference.Target{
+					Addr:                   targetCtx.ParentAddress,
+					Name:                   targetCtx.FriendlyName,
+					Type:                   cty.List(elemType),
+					ScopeId:                targetCtx.ScopeId,
+					RangePtr:               list.expr.Range().Ptr(),
+					NestedTargets:          elemTargets,
+					LocalAddr:              targetCtx.ParentLocalAddress,
+					TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+				})
+			}
+		}
+
+		// type-unaware
+		if targetCtx.AsReference {
+			targets = append(targets, reference.Target{
+				Addr:                   targetCtx.ParentAddress,
+				Name:                   targetCtx.FriendlyName,
+				ScopeId:                targetCtx.ScopeId,
+				RangePtr:               list.expr.Range().Ptr(),
+				NestedTargets:          elemTargets,
+				LocalAddr:              targetCtx.ParentLocalAddress,
+				TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+			})
+		}
+	} else {
+		// treat element targets as 1st class ones
+		// if the list itself isn't targetable
+		targets = elemTargets
 	}
 
 	return targets
