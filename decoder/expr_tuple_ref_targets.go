2bb0fb705855b4d52b140b89aeba6ed710809825
Radek Simko <radek.simko@gmail.com>
2023-03-20 09:11:46 +0000

    decoder: Finish implementing reference targets for Tuple

--- decoder/expr_tuple_ref_targets.go
+++ decoder/expr_tuple_ref_targets.go
@@ -5,26 +5,23 @@ import (
 
 	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/reference"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func (tuple Tuple) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) reference.Targets {
-	eType, ok := tuple.expr.(*hclsyntax.TupleConsExpr)
-	if !ok {
+	elems, diags := hcl.ExprList(tuple.expr)
+	if diags.HasErrors() {
 		return reference.Targets{}
 	}
 
-	if len(eType.Exprs) == 0 || len(tuple.cons.Elems) == 0 {
+	if len(tuple.cons.Elems) == 0 {
 		return reference.Targets{}
 	}
 
-	targets := make(reference.Targets, 0)
+	elemTargets := make(reference.Targets, 0)
 
-	// TODO: collect parent target for the whole tuple
-	// See https://github.com/hashicorp/hcl-lang/issues/228
-
-	for i, elemExpr := range eType.Exprs {
+	for i, elemExpr := range elems {
 		if i+1 > len(tuple.cons.Elems) {
 			break
 		}
@@ -40,8 +37,48 @@ func (tuple Tuple) ReferenceTargets(ctx context.Context, targetCtx *TargetContex
 					Key: cty.NumberIntVal(int64(i)),
 				})
 			}
-			targets = append(targets, e.ReferenceTargets(ctx, elemCtx)...)
+			elemTargets = append(elemTargets, e.ReferenceTargets(ctx, elemCtx)...)
 		}
+	}
+
+	targets := make(reference.Targets, 0)
+
+	if targetCtx != nil {
+		// collect target for the whole tuple
+
+		// type-aware
+		elemType, ok := tuple.cons.ConstraintType()
+		if targetCtx.AsExprType && ok {
+			if ok {
+				targets = append(targets, reference.Target{
+					Addr:                   targetCtx.ParentAddress,
+					Name:                   targetCtx.FriendlyName,
+					Type:                   elemType,
+					ScopeId:                targetCtx.ScopeId,
+					RangePtr:               tuple.expr.Range().Ptr(),
+					NestedTargets:          elemTargets,
+					LocalAddr:              targetCtx.ParentLocalAddress,
+					TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+				})
+			}
+		}
+
+		// type-unaware
+		if targetCtx.AsReference {
+			targets = append(targets, reference.Target{
+				Addr:                   targetCtx.ParentAddress,
+				Name:                   targetCtx.FriendlyName,
+				ScopeId:                targetCtx.ScopeId,
+				RangePtr:               tuple.expr.Range().Ptr(),
+				NestedTargets:          elemTargets,
+				LocalAddr:              targetCtx.ParentLocalAddress,
+				TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+			})
+		}
+	} else {
+		// treat element targets as 1st class ones
+		// if the tuple itself isn't targetable
+		targets = elemTargets
 	}
 
 	return targets
