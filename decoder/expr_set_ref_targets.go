8f7d24994f499297f0c3a0213768c95841f67b4a
Radek Simko <radek.simko@gmail.com>
2023-03-20 09:11:40 +0000

    decoder: Finish implementing reference targets for Set

--- decoder/expr_set_ref_targets.go
+++ decoder/expr_set_ref_targets.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl-lang/reference"
+	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func (set Set) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) reference.Targets {
@@ -19,8 +21,38 @@ func (set Set) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) r
 
 	targets := make(reference.Targets, 0)
 
-	// TODO: collect parent target for the whole set
-	// See https://github.com/hashicorp/hcl-lang/issues/228
+	if targetCtx != nil {
+		// collect target for the whole set
+
+		// type-aware
+		elemCons, ok := set.cons.Elem.(schema.TypeAwareConstraint)
+		if targetCtx.AsExprType && ok {
+			elemType, ok := elemCons.ConstraintType()
+			if ok {
+				targets = append(targets, reference.Target{
+					Addr:                   targetCtx.ParentAddress,
+					Name:                   targetCtx.FriendlyName,
+					Type:                   cty.Set(elemType),
+					ScopeId:                targetCtx.ScopeId,
+					RangePtr:               set.expr.Range().Ptr(),
+					LocalAddr:              targetCtx.ParentLocalAddress,
+					TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+				})
+			}
+		}
+
+		// type-unaware
+		if targetCtx.AsReference {
+			targets = append(targets, reference.Target{
+				Addr:                   targetCtx.ParentAddress,
+				Name:                   targetCtx.FriendlyName,
+				ScopeId:                targetCtx.ScopeId,
+				RangePtr:               set.expr.Range().Ptr(),
+				LocalAddr:              targetCtx.ParentLocalAddress,
+				TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+			})
+		}
+	}
 
 	return targets
 }
