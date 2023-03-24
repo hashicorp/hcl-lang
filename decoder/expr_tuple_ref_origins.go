f229b9dfddcc91ef9b5e4b57c7e78ddf55680122
Radek Simko <radek.simko@gmail.com>
2023-03-24 18:46:29 +0000

    decoder: Finish implementing reference origins for Tuple

--- decoder/expr_tuple_ref_origins.go
+++ decoder/expr_tuple_ref_origins.go
@@ -4,22 +4,22 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl-lang/reference"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"
 )
 
 func (tuple Tuple) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
-	eType, ok := tuple.expr.(*hclsyntax.TupleConsExpr)
-	if !ok {
+	elems, diags := hcl.ExprList(tuple.expr)
+	if diags.HasErrors() {
 		return reference.Origins{}
 	}
 
-	if len(eType.Exprs) == 0 || len(tuple.cons.Elems) == 0 {
+	if len(elems) == 0 || len(tuple.cons.Elems) == 0 {
 		return reference.Origins{}
 	}
 
 	origins := make(reference.Origins, 0)
 
-	for i, elemExpr := range eType.Exprs {
+	for i, elemExpr := range elems {
 		if i+1 > len(tuple.cons.Elems) {
 			break
 		}
