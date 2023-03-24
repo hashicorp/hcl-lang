3a6053ff42e61c76322af9975e461f9f6f0771e3
Radek Simko <radek.simko@gmail.com>
2023-03-24 09:35:03 +0000

    decoder: Account for JSON in ref. targets for LiteralType

--- decoder/expr_literal_type_ref_targets.go
+++ decoder/expr_literal_type_ref_targets.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hashicorp/hcl-lang/reference"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -63,47 +62,32 @@ func (lt LiteralType) ReferenceTargets(ctx context.Context, targetCtx *TargetCon
 	}
 
 	if typ.IsListType() {
-		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
-		if !ok {
-			return nil
-		}
-
 		list := List{
 			cons: schema.List{
 				Elem: schema.LiteralType{
 					Type: typ.ElementType(),
 				},
 			},
-			expr:    expr,
+			expr:    lt.expr,
 			pathCtx: lt.pathCtx,
 		}
 		return list.ReferenceTargets(ctx, targetCtx)
 	}
 
 	if typ.IsSetType() {
-		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
-		if !ok {
-			return nil
-		}
-
 		set := Set{
 			cons: schema.Set{
 				Elem: schema.LiteralType{
 					Type: typ.ElementType(),
 				},
 			},
-			expr:    expr,
+			expr:    lt.expr,
 			pathCtx: lt.pathCtx,
 		}
 		return set.ReferenceTargets(ctx, targetCtx)
 	}
 
 	if typ.IsTupleType() {
-		expr, ok := lt.expr.(*hclsyntax.TupleConsExpr)
-		if !ok {
-			return nil
-		}
-
 		elemTypes := typ.TupleElementTypes()
 		cons := schema.Tuple{
 			Elems: make([]schema.Constraint, len(elemTypes)),
@@ -115,7 +99,7 @@ func (lt LiteralType) ReferenceTargets(ctx context.Context, targetCtx *TargetCon
 		}
 		tuple := Tuple{
 			cons:    cons,
-			expr:    expr,
+			expr:    lt.expr,
 			pathCtx: lt.pathCtx,
 		}
 
@@ -123,34 +107,24 @@ func (lt LiteralType) ReferenceTargets(ctx context.Context, targetCtx *TargetCon
 	}
 
 	if typ.IsMapType() {
-		expr, ok := lt.expr.(*hclsyntax.ObjectConsExpr)
-		if !ok {
-			return nil
-		}
-
 		m := Map{
 			cons: schema.Map{
 				Elem: schema.LiteralType{
 					Type: typ.ElementType(),
 				},
 			},
-			expr:    expr,
+			expr:    lt.expr,
 			pathCtx: lt.pathCtx,
 		}
 		return m.ReferenceTargets(ctx, targetCtx)
 	}
 
 	if typ.IsObjectType() {
-		expr, ok := lt.expr.(*hclsyntax.ObjectConsExpr)
-		if !ok {
-			return nil
-		}
-
 		obj := Object{
 			cons: schema.Object{
 				Attributes: ctyObjectToObjectAttributes(typ),
 			},
-			expr:    expr,
+			expr:    lt.expr,
 			pathCtx: lt.pathCtx,
 		}
 		return obj.ReferenceTargets(ctx, targetCtx)
