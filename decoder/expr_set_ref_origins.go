9795032cdc56b5b68feb6ed6e801454191cbf930
Radek Simko <radek.simko@gmail.com>
2023-03-24 18:46:09 +0000

    decoder: Finish implementing reference origins for Set

--- decoder/expr_set_ref_origins.go
+++ decoder/expr_set_ref_origins.go
@@ -4,22 +4,22 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl-lang/reference"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"
 )
 
 func (set Set) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
-	eType, ok := set.expr.(*hclsyntax.TupleConsExpr)
-	if !ok {
+	elems, diags := hcl.ExprList(set.expr)
+	if diags.HasErrors() {
 		return reference.Origins{}
 	}
 
-	if len(eType.Exprs) == 0 || set.cons.Elem == nil {
+	if len(elems) == 0 || set.cons.Elem == nil {
 		return reference.Origins{}
 	}
 
 	origins := make(reference.Origins, 0)
 
-	for _, elemExpr := range eType.Exprs {
+	for _, elemExpr := range elems {
 		expr := newExpression(set.pathCtx, elemExpr, set.cons.Elem)
 		if e, ok := expr.(ReferenceOriginsExpression); ok {
 			origins = append(origins, e.ReferenceOrigins(ctx, allowSelfRefs)...)
