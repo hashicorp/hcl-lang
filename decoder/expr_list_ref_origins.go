3596e57734890acc157191cf40a863be0a991200
Radek Simko <radek.simko@gmail.com>
2023-03-24 18:46:00 +0000

    decoder: Finish implementing reference origins for List

--- decoder/expr_list_ref_origins.go
+++ decoder/expr_list_ref_origins.go
@@ -4,22 +4,22 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl-lang/reference"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"
 )
 
 func (list List) ReferenceOrigins(ctx context.Context, allowSelfRefs bool) reference.Origins {
-	eType, ok := list.expr.(*hclsyntax.TupleConsExpr)
-	if !ok {
+	elems, diags := hcl.ExprList(list.expr)
+	if diags.HasErrors() {
 		return reference.Origins{}
 	}
 
-	if len(eType.Exprs) == 0 || list.cons.Elem == nil {
+	if len(elems) == 0 || list.cons.Elem == nil {
 		return reference.Origins{}
 	}
 
 	origins := make(reference.Origins, 0)
 
-	for _, elemExpr := range eType.Exprs {
+	for _, elemExpr := range elems {
 		expr := newExpression(list.pathCtx, elemExpr, list.cons.Elem)
 		if e, ok := expr.(ReferenceOriginsExpression); ok {
 			origins = append(origins, e.ReferenceOrigins(ctx, allowSelfRefs)...)
