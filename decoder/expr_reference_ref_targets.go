25b482bd8c2b23900f76f9039a08e76452cb0605
Radek Simko <radek.simko@gmail.com>
2023-03-28 17:36:29 +0100

    decoder: Account for legacy JSON syntax in Reference

--- decoder/expr_reference_ref_targets.go
+++ decoder/expr_reference_ref_targets.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/json"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func (ref Reference) ReferenceTargets(ctx context.Context, _ *TargetContext) reference.Targets {
@@ -38,41 +39,73 @@ func (ref Reference) ReferenceTargets(ctx context.Context, _ *TargetContext) ref
 		// guess whether the expression has exactly a single traversal
 
 		vars := ref.expr.Variables()
-		if len(vars) != 1 {
+		if len(vars) == 1 {
+			tRange := vars[0].SourceRange()
+			expectedExprRange := hcl.Range{
+				Filename: tRange.Filename,
+				Start: hcl.Pos{
+					Line: tRange.Start.Line,
+					// account for "${
+					Column: tRange.Start.Column - 3,
+					Byte:   tRange.Start.Byte - 3,
+				},
+				End: hcl.Pos{
+					Line: tRange.End.Line,
+					// account for }"
+					Column: tRange.End.Column + 2,
+					Byte:   tRange.End.Byte + 2,
+				},
+			}
+
+			if rangesEqual(expectedExprRange, ref.expr.Range()) {
+				addr, err := lang.TraversalToAddress(vars[0])
+				if err != nil {
+					return reference.Targets{}
+				}
+
+				return reference.Targets{
+					reference.Target{
+						Addr:     addr,
+						ScopeId:  ref.cons.Address.ScopeId,
+						RangePtr: vars[0].SourceRange().Ptr(),
+						Name:     ref.cons.Name,
+					},
+				}
+			}
+		}
+
+		// Account for "legacy" string syntax which is still
+		// in use by Terraform to date in this context.
+		val, diags := ref.expr.Value(&hcl.EvalContext{})
+		if diags.HasErrors() {
+			return reference.Targets{}
+		}
+		if val.Type() != cty.String {
+			return reference.Targets{}
+		}
+		startPos := hcl.Pos{
+			Line: ref.expr.Range().Start.Line,
+			// Account for the leading double quote
+			Column: ref.expr.Range().Start.Column + 1,
+			Byte:   ref.expr.Range().Start.Byte + 1,
+		}
+
+		traversal, diags := hclsyntax.ParseTraversalAbs([]byte(val.AsString()), ref.expr.Range().Filename, startPos)
+		if diags.HasErrors() {
+			return reference.Targets{}
+		}
+		addr, err := lang.TraversalToAddress(traversal)
+		if err != nil {
 			return reference.Targets{}
 		}
 
-		tRange := vars[0].SourceRange()
-		expectedExprRange := hcl.Range{
-			Filename: tRange.Filename,
-			Start: hcl.Pos{
-				Line: tRange.Start.Line,
-				// account for "${
-				Column: tRange.Start.Column - 3,
-				Byte:   tRange.Start.Byte - 3,
+		return reference.Targets{
+			reference.Target{
+				Addr:     addr,
+				ScopeId:  ref.cons.Address.ScopeId,
+				RangePtr: traversal.SourceRange().Ptr(),
+				Name:     ref.cons.Name,
 			},
-			End: hcl.Pos{
-				Line: tRange.End.Line,
-				// account for }"
-				Column: tRange.End.Column + 2,
-				Byte:   tRange.End.Byte + 2,
-			},
-		}
-
-		if rangesEqual(expectedExprRange, ref.expr.Range()) {
-			addr, err := lang.TraversalToAddress(vars[0])
-			if err != nil {
-				return reference.Targets{}
-			}
-
-			return reference.Targets{
-				reference.Target{
-					Addr:     addr,
-					ScopeId:  ref.cons.Address.ScopeId,
-					RangePtr: vars[0].SourceRange().Ptr(),
-					Name:     ref.cons.Name,
-				},
-			}
 		}
 	}
 
