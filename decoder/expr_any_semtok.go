e00d6f82d4d1f709e92bcfd7c28b4e4c2bcc37c2
Radek Simko <radek.simko@gmail.com>
2023-04-03 12:40:01 +0100

    decoder: Implement semantic tokens for AnyExpression

--- decoder/expr_any_semtok.go
+++ decoder/expr_any_semtok.go
@@ -4,28 +4,124 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 func (a Any) SemanticTokens(ctx context.Context) []lang.SemanticToken {
-	switch e := a.expr.(type) {
-	// TODO! Support LiteralType
-	// TODO! Support References
-	case *hclsyntax.FunctionCallExpr:
-		_, ok := a.pathCtx.Functions[e.Name]
-		if ok {
-			// TODO! check for name range
-			// TODO! loop over arguments, do SemanticTokens for that argument using the constraint
-			// TODO? if we introduce a new token type, we need to add it in the extension's package.json and docs
-			return []lang.SemanticToken{
-				{
-					Type:      lang.TokenAttrName,
-					Modifiers: []lang.SemanticTokenModifier{},
-					// Range:     eType.Range(),
-				},
-			}
+	typ := a.cons.OfType
+
+	if typ.IsListType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
 		}
+
+		cons := schema.List{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+
+		return newExpression(a.pathCtx, expr, cons).SemanticTokens(ctx)
 	}
 
-	return nil
+	if typ.IsSetType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Set{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+
+		return newExpression(a.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsTupleType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		elemTypes := typ.TupleElementTypes()
+		cons := schema.Tuple{
+			Elems: make([]schema.Constraint, len(elemTypes)),
+		}
+		for i, elemType := range elemTypes {
+			cons.Elems[i] = schema.AnyExpression{
+				OfType: elemType,
+			}
+		}
+
+		return newExpression(a.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsMapType() {
+		expr, ok := a.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Map{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+		return newExpression(a.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	if typ.IsObjectType() {
+		expr, ok := a.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return []lang.SemanticToken{}
+		}
+
+		cons := schema.Object{
+			Attributes: ctyObjectToObjectAttributes(typ),
+		}
+		return newExpression(a.pathCtx, expr, cons).SemanticTokens(ctx)
+	}
+
+	return a.semanticTokensForNonComplexExpr(ctx)
+}
+
+func (a Any) semanticTokensForNonComplexExpr(ctx context.Context) []lang.SemanticToken {
+	// TODO: Support TemplateExpr https://github.com/hashicorp/terraform-ls/issues/522
+	// TODO: Support splat expression https://github.com/hashicorp/terraform-ls/issues/526
+	// TODO: Support for-in-if expression https://github.com/hashicorp/terraform-ls/issues/527
+	// TODO: Support conditional expression https://github.com/hashicorp/terraform-ls/issues/528
+	// TODO: Support operator expresssions https://github.com/hashicorp/terraform-ls/issues/529
+	// TODO: Support complex index expressions https://github.com/hashicorp/terraform-ls/issues/531
+	// TODO: Support relative traversals https://github.com/hashicorp/terraform-ls/issues/532
+
+	ref := Reference{
+		expr:    a.expr,
+		cons:    schema.Reference{OfType: a.cons.OfType},
+		pathCtx: a.pathCtx,
+	}
+	tokens := ref.SemanticTokens(ctx)
+	if tokens != nil {
+		return tokens
+	}
+
+	fe := functionExpr{
+		expr:       a.expr,
+		returnType: a.cons.OfType,
+		pathCtx:    a.pathCtx,
+	}
+	tokens = fe.SemanticTokens(ctx)
+	if tokens != nil {
+		return tokens
+	}
+
+	lt := LiteralType{
+		expr:    a.expr,
+		cons:    schema.LiteralType{Type: a.cons.OfType},
+		pathCtx: a.pathCtx,
+	}
+	return lt.SemanticTokens(ctx)
 }
