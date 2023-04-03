721fa94797a9719da68a22e9e1f734cb4a4131e1
Radek Simko <radek.simko@gmail.com>
2023-04-03 12:39:47 +0100

    decoder: Implement hover for AnyExpression

--- decoder/expr_any_hover.go
+++ decoder/expr_any_hover.go
@@ -2,29 +2,127 @@ package decoder
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 func (a Any) HoverAtPos(ctx context.Context, pos hcl.Pos) *lang.HoverData {
-	switch e := a.expr.(type) {
-	// TODO! Support LiteralType
-	// TODO! Support References
-	case *hclsyntax.FunctionCallExpr:
-		f, ok := a.pathCtx.Functions[e.Name]
-		if ok {
-			// TODO! check for name range
-			// TODO! loop over arguments, do hoverAtPos for that argument using the constraint
-			return &lang.HoverData{
-				// TODO? add link to docs
-				Content: lang.Markdown(fmt.Sprintf("```terraform\n%s(%s) %s\n```\n\n%s", e.Name, parameterNamesAsString(f), f.ReturnType.FriendlyName(), f.Description)),
-				Range:   a.expr.Range(),
-			}
+	typ := a.cons.OfType
+
+	if typ.IsListType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return a.hoverNonComplexExprAtPos(ctx, pos)
 		}
+
+		cons := schema.List{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+
+		return newExpression(a.pathCtx, expr, cons).HoverAtPos(ctx, pos)
 	}
 
-	return nil
+	if typ.IsSetType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return a.hoverNonComplexExprAtPos(ctx, pos)
+		}
+
+		cons := schema.Set{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+
+		return newExpression(a.pathCtx, expr, cons).HoverAtPos(ctx, pos)
+	}
+
+	if typ.IsTupleType() {
+		expr, ok := a.expr.(*hclsyntax.TupleConsExpr)
+		if !ok {
+			return a.hoverNonComplexExprAtPos(ctx, pos)
+		}
+
+		elemTypes := typ.TupleElementTypes()
+		cons := schema.Tuple{
+			Elems: make([]schema.Constraint, len(elemTypes)),
+		}
+		for i, elemType := range elemTypes {
+			cons.Elems[i] = schema.LiteralType{
+				Type: elemType,
+			}
+		}
+
+		return newExpression(a.pathCtx, expr, cons).HoverAtPos(ctx, pos)
+	}
+
+	if typ.IsMapType() {
+		expr, ok := a.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return a.hoverNonComplexExprAtPos(ctx, pos)
+		}
+
+		cons := schema.Map{
+			Elem: schema.AnyExpression{
+				OfType: typ.ElementType(),
+			},
+		}
+		return newExpression(a.pathCtx, expr, cons).HoverAtPos(ctx, pos)
+	}
+
+	if typ.IsObjectType() {
+		expr, ok := a.expr.(*hclsyntax.ObjectConsExpr)
+		if !ok {
+			return a.hoverNonComplexExprAtPos(ctx, pos)
+		}
+
+		cons := schema.Object{
+			Attributes: ctyObjectToObjectAttributes(typ),
+		}
+		return newExpression(a.pathCtx, expr, cons).HoverAtPos(ctx, pos)
+	}
+
+	return a.hoverNonComplexExprAtPos(ctx, pos)
+}
+
+func (a Any) hoverNonComplexExprAtPos(ctx context.Context, pos hcl.Pos) *lang.HoverData {
+	// TODO: Support TemplateExpr https://github.com/hashicorp/terraform-ls/issues/522
+	// TODO: Support splat expression https://github.com/hashicorp/terraform-ls/issues/526
+	// TODO: Support for-in-if expression https://github.com/hashicorp/terraform-ls/issues/527
+	// TODO: Support conditional expression https://github.com/hashicorp/terraform-ls/issues/528
+	// TODO: Support operator expresssions https://github.com/hashicorp/terraform-ls/issues/529
+	// TODO: Support complex index expressions https://github.com/hashicorp/terraform-ls/issues/531
+	// TODO: Support relative traversals https://github.com/hashicorp/terraform-ls/issues/532
+
+	ref := Reference{
+		expr:    a.expr,
+		cons:    schema.Reference{OfType: a.cons.OfType},
+		pathCtx: a.pathCtx,
+	}
+	hoverData := ref.HoverAtPos(ctx, pos)
+	if hoverData != nil {
+		return hoverData
+	}
+
+	fe := functionExpr{
+		expr:       a.expr,
+		returnType: a.cons.OfType,
+		pathCtx:    a.pathCtx,
+	}
+	hoverData = fe.HoverAtPos(ctx, pos)
+	if hoverData != nil {
+		return hoverData
+	}
+
+	lt := LiteralType{
+		expr:    a.expr,
+		cons:    schema.LiteralType{Type: a.cons.OfType},
+		pathCtx: a.pathCtx,
+	}
+	return lt.HoverAtPos(ctx, pos)
 }
