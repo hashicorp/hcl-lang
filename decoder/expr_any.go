2fb6cc9ae22bf3016898fe2a1fb0d985b21dd6e3
Radek Simko <radek.simko@gmail.com>
2023-04-03 21:26:13 +0100

    decoder: Implement InferType() for Any

--- decoder/expr_any.go
+++ decoder/expr_any.go
@@ -6,10 +6,27 @@ package decoder
 import (
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type Any struct {
 	expr    hcl.Expression
 	cons    schema.AnyExpression
 	pathCtx *PathContext
+}
+
+func (a Any) InferType() (cty.Type, bool) {
+	consType, ok := a.cons.ConstraintType()
+	if !ok {
+		return consType, false
+	}
+
+	if consType == cty.DynamicPseudoType && !isEmptyExpression(a.expr) {
+		val, diags := a.expr.Value(nil)
+		if !diags.HasErrors() {
+			consType = val.Type()
+		}
+	}
+
+	return consType, true
 }
