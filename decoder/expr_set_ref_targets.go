334f66cb2529adf5e8b06e6bda39571a49c8746b
Radek Simko <radek.simko@gmail.com>
2023-04-05 21:48:48 +0100

    decoder: Collect Set as implied target

--- decoder/expr_set_ref_targets.go
+++ decoder/expr_set_ref_targets.go
@@ -13,6 +13,10 @@ import (
 )
 
 func (set Set) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) reference.Targets {
+	if isEmptyExpression(set.expr) && targetCtx != nil {
+		return set.wholeSetReferenceTargets(targetCtx, nil)
+	}
+
 	elems, diags := hcl.ExprList(set.expr)
 	if diags.HasErrors() {
 		return reference.Targets{}
@@ -36,55 +40,58 @@ func (set Set) ReferenceTargets(ctx context.Context, targetCtx *TargetContext) r
 		}
 	}
 
+	if targetCtx == nil {
+		// treat element targets as 1st class ones
+		// if the list itself isn't targetable
+		return elemTargets
+	}
+
+	return set.wholeSetReferenceTargets(targetCtx, elemTargets)
+}
+
+func (set Set) wholeSetReferenceTargets(targetCtx *TargetContext, nestedTargets reference.Targets) reference.Targets {
 	targets := make(reference.Targets, 0)
 
-	if targetCtx != nil {
-		// collect target for the whole set
+	// collect target for the whole set
 
-		var rangePtr *hcl.Range
-		if targetCtx.ParentRangePtr != nil {
-			rangePtr = targetCtx.ParentRangePtr
-		} else {
-			rangePtr = set.expr.Range().Ptr()
-		}
+	var rangePtr *hcl.Range
+	if targetCtx.ParentRangePtr != nil {
+		rangePtr = targetCtx.ParentRangePtr
+	} else {
+		rangePtr = set.expr.Range().Ptr()
+	}
 
-		// type-aware
-		elemCons, ok := set.cons.Elem.(schema.TypeAwareConstraint)
-		if targetCtx.AsExprType && ok {
-			elemType, ok := elemCons.ConstraintType()
-			if ok {
-				targets = append(targets, reference.Target{
-					Addr:                   targetCtx.ParentAddress,
-					Name:                   targetCtx.FriendlyName,
-					Type:                   cty.Set(elemType),
-					ScopeId:                targetCtx.ScopeId,
-					RangePtr:               rangePtr,
-					DefRangePtr:            targetCtx.ParentDefRangePtr,
-					NestedTargets:          elemTargets,
-					LocalAddr:              targetCtx.ParentLocalAddress,
-					TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
-				})
-			}
-		}
-
-		// type-unaware
-		if targetCtx.AsReference {
+	// type-aware
+	elemCons, ok := set.cons.Elem.(schema.TypeAwareConstraint)
+	if targetCtx.AsExprType && ok {
+		elemType, ok := elemCons.ConstraintType()
+		if ok {
 			targets = append(targets, reference.Target{
 				Addr:                   targetCtx.ParentAddress,
 				Name:                   targetCtx.FriendlyName,
+				Type:                   cty.Set(elemType),
 				ScopeId:                targetCtx.ScopeId,
 				RangePtr:               rangePtr,
 				DefRangePtr:            targetCtx.ParentDefRangePtr,
-				NestedTargets:          elemTargets,
+				NestedTargets:          nestedTargets,
 				LocalAddr:              targetCtx.ParentLocalAddress,
 				TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
 			})
 		}
-	} else {
-		// treat element targets as 1st class ones
-		// if the list itself isn't targetable
-		targets = elemTargets
 	}
 
+	// type-unaware
+	if targetCtx.AsReference {
+		targets = append(targets, reference.Target{
+			Addr:                   targetCtx.ParentAddress,
+			Name:                   targetCtx.FriendlyName,
+			ScopeId:                targetCtx.ScopeId,
+			RangePtr:               rangePtr,
+			DefRangePtr:            targetCtx.ParentDefRangePtr,
+			NestedTargets:          nestedTargets,
+			LocalAddr:              targetCtx.ParentLocalAddress,
+			TargetableFromRangePtr: targetCtx.TargetableFromRangePtr,
+		})
+	}
 	return targets
 }
