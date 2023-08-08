1828ecdd42fb514740e57cbc64ad642fc01ef131
James Pogran <jpogran@outlook.com>
2023-08-08 05:27:09 -0400

    Schema based validation (#294)
    * unknown attribute
    
    * deprecated attribute
    
    * deprecated block
    
    * label validation
    
    * max and min items in blocks
    
    * formatting
    
    * check for required attributes
    
    * docs
    
    * min/max blocks
    
    * formatting
    
    * Update validate API with map of diagnostics
    
    * todo tests
    
    * use new validate API and account for min/max items with no blocks specified
    
    * formatting
    
    * edge cases
    
    * remove unneeded declarations
    
    * fix test ordering
    
    * fix test ordering
    
    * use slicestable
    
    * use slicestable
    
    * cleanup: avoid appending inside helper functions for readability
    
    ---------
    
    Co-authored-by: Daniel Banck <daniel@dbanck.de>
    Co-authored-by: Radek Simko <radek.simko@gmail.com>

--- decoder/validate.go
+++ decoder/validate.go
@@ -5,54 +5,154 @@ package decoder
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
-func (d *PathDecoder) Validate(ctx context.Context) (hcl.Diagnostics, error) {
+// Validate returns a set of Diagnostics for all known files
+func (d *PathDecoder) Validate(ctx context.Context) (map[string]hcl.Diagnostics, error) {
+	diags := make(map[string]hcl.Diagnostics, 0)
 	if d.pathCtx.Schema == nil {
-		return hcl.Diagnostics{}, &NoSchemaError{}
+		return diags, &NoSchemaError{}
 	}
 
-	diags := hcl.Diagnostics{}
 	// Validate module files per schema
-	for _, f := range d.pathCtx.Files {
+	for filename, f := range d.pathCtx.Files {
 		body, ok := f.Body.(*hclsyntax.Body)
 		if !ok {
 			// TODO! error
 			continue
 		}
 
-		diags = diags.Extend(d.validateBody(ctx, body, d.pathCtx.Schema))
+		diags[filename] = d.validateBody(ctx, body, d.pathCtx.Schema)
 	}
 
 	// Run validation functions
-	for _, vFunc := range d.decoderCtx.Validations {
-		diags = diags.Extend(vFunc(ctx))
-	}
+	// for _, vFunc := range d.decoderCtx.Validations {
+	// 	diags = diags.Extend(vFunc(ctx))
+	// }
 
 	return diags, nil
 }
 
+// validateBody returns a set of Diagnostics for a given HCL body
+//
+// Validations available:
+//
+//   - unexpected attribute
+//
+//   - missing required attribute
+//
+//   - deprecated attribute
+//
+//   - unexpected block
+//
+//   - deprecated block
+//
+//   - min blocks
+//
+//   - max blocks
 func (d *PathDecoder) validateBody(ctx context.Context, body *hclsyntax.Body, bodySchema *schema.BodySchema) hcl.Diagnostics {
 	diags := hcl.Diagnostics{}
 
-	for name, _ := range body.Attributes {
-		_, ok := bodySchema.Attributes[name]
+	// Iterate over all Attributes in the body
+	for name, attribute := range body.Attributes {
+		attributeSchema, ok := bodySchema.Attributes[name]
 		if !ok {
-			// TODO! unknown attribute validation
+			// ---------- diag ERR unknown attribute
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Unexpected attribute",
+				Detail:   fmt.Sprintf("An attribute named %q is not expected here", name),
+				Subject:  attribute.SrcRange.Ptr(),
+			})
+			// don't check futher because this isn't a valid attribute
+			continue
 		}
-		// TODO! validate against schema
+
+		// ---------- diag WARN deprecated attribute
+		if attributeSchema.IsDeprecated {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagWarning,
+				Summary:  fmt.Sprintf("%q is deprecated", name),
+				Detail:   fmt.Sprintf("Reason: %q", attributeSchema.Description.Value),
+				Subject:  attribute.SrcRange.Ptr(),
+			})
+		}
 	}
 
+	// Iterate over all schema Attributes and check if specified in the configuration
+	for name, attribute := range bodySchema.Attributes {
+		if attribute.IsRequired {
+			_, ok := body.Attributes[name]
+			if !ok {
+				// ---------- diag ERR unknown attribute
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  fmt.Sprintf("Required attribute %q not specified", name),
+					Detail:   fmt.Sprintf("An attribute named %q is required here", name),
+					// TODO This is the closest I could think of
+					// maybe block instead ?
+					Subject: body.SrcRange.Ptr(),
+				})
+			}
+		}
+	}
+
+	// keep track of blocks actually used so we can compare to schema later
+	specifiedBlocks := make(map[string]int)
+
+	// Iterate over all Blocks in the body
 	for _, block := range body.Blocks {
 		blockSchema, ok := bodySchema.Blocks[block.Type]
 		if !ok {
-			// TODO! unknown block validation
+			// ---------- diag ERR unknown block
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Unexpected block",
+				Detail:   fmt.Sprintf("Blocks of type %q are not expected here", block.Type),
+				Subject:  block.TypeRange.Ptr(),
+			})
+			// don't check futher because this isn't a valid block
+			continue
 		}
-		// TODO! validate against schema
+
+		// ---------- diag WARN deprecated block
+		if blockSchema.IsDeprecated {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagWarning,
+				Summary:  fmt.Sprintf("%q is deprecated", block.Type),
+				// todo check if description is there
+				Detail:  fmt.Sprintf("Reason: %q", blockSchema.Description.Value),
+				Subject: &block.TypeRange,
+			})
+		}
+
+		// ---------- daig ERR extraneous block labels
+		validLabelNum := len(blockSchema.Labels)
+		for i := range block.Labels {
+			if i >= validLabelNum {
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  fmt.Sprintf("Too many labels specified for %q", block.Type),
+					Detail:   fmt.Sprintf("Only %d label(s) are expected for %q blocks", validLabelNum, block.Type),
+					Subject:  block.LabelRanges[i].Ptr(),
+				})
+			}
+		}
+
+		// ---------- diag ERR missing labels
+		if validLabelNum > len(block.Labels) {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  fmt.Sprintf("Not enough labels specified for %q", block.Type),
+				Detail:   fmt.Sprintf("All %q blocks must have %d label(s)", block.Type, validLabelNum),
+				Subject:  block.TypeRange.Ptr(),
+			})
+		}
 
 		if block.Body != nil {
 			mergedSchema, err := mergeBlockBodySchemas(block.AsHCLBlock(), blockSchema)
@@ -60,9 +160,85 @@ func (d *PathDecoder) validateBody(ctx context.Context, body *hclsyntax.Body, bo
 				// TODO! err
 			}
 
+			// Recurse for nested blocks
 			diags = diags.Extend(d.validateBody(ctx, block.Body, mergedSchema))
+		}
+
+		// build list of blocks specified
+		specifiedBlocks[block.Type]++
+	}
+
+	// Iterate over bodySchema Blocks and check if they are specified in configuration
+	for name, block := range bodySchema.Blocks {
+		// check if the bodySchema Block is specified in the configuration
+		numBlocks, ok := specifiedBlocks[name]
+		if ok {
+			// block is in schema and user specified it in configuration
+			// check if schema says there should be maximum number of items for this block
+			if block.MaxItems > 0 {
+				// ---------- diag ERR too many blocks
+				if numBlocks > int(block.MaxItems) {
+					subjectRange := &body.Blocks[block.Type].TypeRange
+					maxItems := block.MaxItems
+					diags = append(diags, tooManyBlocksDiag(diags, name, maxItems, subjectRange))
+				}
+			}
+
+			// check if schema says there should be minimum number of items for this block
+			if block.MinItems > 0 {
+				// ---------- diag ERR too little blocks
+				if numBlocks < int(block.MinItems) {
+					subjectRange := &body.Blocks[block.Type].TypeRange
+					minItems := block.MinItems
+					diags = append(diags, tooFewItemsDiag(diags, name, minItems, subjectRange))
+				}
+			}
+		} else {
+			// block is in schema, but user did not specify it in configuration
+			// check if schema says there should be maximum number of items for this block
+			numBlocks = 0
+			if block.MaxItems > 0 {
+				// ---------- diag ERR too many blocks
+				if numBlocks > int(block.MaxItems) {
+					// use current body range as there isn't a block to reference because
+					// the user didn't write anything here
+					subjectRange := &body.SrcRange
+					maxItems := block.MaxItems
+					diags = append(diags, tooManyBlocksDiag(diags, name, maxItems, subjectRange))
+				}
+			}
+
+			// check if schema says there should be minimum number of items for this block
+			if block.MinItems > 0 {
+				// ---------- diag ERR too little blocks
+				if numBlocks < int(block.MinItems) {
+					// use current body range as there isn't a block to reference because
+					// the user didn't write anything here
+					subjectRange := &body.SrcRange
+					minItems := block.MinItems
+					diags = append(diags, tooFewItemsDiag(diags, name, minItems, subjectRange))
+				}
+			}
 		}
 	}
 
 	return diags
+}
+
+func tooFewItemsDiag(diags hcl.Diagnostics, name string, minItems uint64, subjectRange *hcl.Range) *hcl.Diagnostic {
+	return &hcl.Diagnostic{
+		Severity: hcl.DiagError,
+		Summary:  fmt.Sprintf("Too few blocks specified for %q", name),
+		Detail:   fmt.Sprintf("At least %d block(s) are expected for %q", minItems, name),
+		Subject:  subjectRange,
+	}
+}
+
+func tooManyBlocksDiag(diags hcl.Diagnostics, name string, maxItems uint64, subjectRange *hcl.Range) *hcl.Diagnostic {
+	return &hcl.Diagnostic{
+		Severity: hcl.DiagError,
+		Summary:  fmt.Sprintf("Too many blocks specified for %q", name),
+		Detail:   fmt.Sprintf("Only %d block(s) are expected for %q", maxItems, name),
+		Subject:  subjectRange,
+	}
 }
--- decoder/validate_test.go
+++ decoder/validate_test.go
@@ -6,9 +6,11 @@ package decoder
 import (
 	"context"
 	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/hcl-lang/lang"
 	"github.com/hashicorp/hcl-lang/schema"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -20,13 +22,15 @@ func TestValidate_schema(t *testing.T) {
 		testName            string
 		bodySchema          *schema.BodySchema
 		cfg                 string
-		expectedDiagnostics hcl.Diagnostics
+		expectedDiagnostics map[string]hcl.Diagnostics
 	}{
 		{
 			"empty schema",
 			schema.NewBodySchema(),
 			``,
-			hcl.Diagnostics{},
+			map[string]hcl.Diagnostics{
+				"test.tf": {},
+			},
 		},
 		{
 			"valid schema",
@@ -39,7 +43,568 @@ func TestValidate_schema(t *testing.T) {
 				},
 			},
 			`test = 1`,
-			hcl.Diagnostics{},
+			map[string]hcl.Diagnostics{
+				"test.tf": {},
+			},
+		},
+		// attributes
+		{
+			"unknown attribute",
+			&schema.BodySchema{
+				Attributes: map[string]*schema.AttributeSchema{
+					"test": {
+						Constraint: schema.LiteralType{Type: cty.Number},
+						IsRequired: true,
+					},
+				},
+			},
+			`test = 1
+	foo = 1`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Unexpected attribute",
+						Detail:   "An attribute named \"foo\" is not expected here",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 2, Byte: 10},
+							End:      hcl.Pos{Line: 2, Column: 9, Byte: 17},
+						},
+					},
+				},
+			},
+		},
+		{
+			"unknown block attribute",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+	test = 1
+	foo = 1
+}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Unexpected attribute",
+						Detail:   "An attribute named \"foo\" is not expected here",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 3, Column: 2, Byte: 17},
+							End:      hcl.Pos{Line: 3, Column: 9, Byte: 24},
+						},
+					},
+				},
+			},
+		},
+		{
+			"deprecated attribute",
+			&schema.BodySchema{
+				Attributes: map[string]*schema.AttributeSchema{
+					"test": {
+						Constraint: schema.LiteralType{Type: cty.Number},
+						IsRequired: true,
+					},
+					"wakka": {
+						Constraint:   schema.LiteralType{Type: cty.Number},
+						IsRequired:   false,
+						IsDeprecated: true,
+						Description: lang.MarkupContent{
+							Value: "Use `wakka_wakka` instead",
+							Kind:  lang.MarkdownKind,
+						},
+					},
+				},
+			},
+			`test = 1
+wakka = 2
+`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagWarning,
+						Summary:  "\"wakka\" is deprecated",
+						Detail:   "Reason: \"Use `wakka_wakka` instead\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 1, Byte: 9},
+							End:      hcl.Pos{Line: 2, Column: 10, Byte: 18},
+						},
+					},
+				},
+			},
+		},
+		// blocks
+		{
+			"missing required attribute",
+			&schema.BodySchema{
+				Attributes: map[string]*schema.AttributeSchema{
+					"wakka": {
+						IsRequired: true,
+						Constraint: schema.LiteralType{Type: cty.String},
+					},
+					"bar": {
+						Constraint: schema.LiteralType{Type: cty.String},
+					},
+				},
+			},
+			`bar = "baz"`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Required attribute \"wakka\" not specified",
+						Detail:   "An attribute named \"wakka\" is required here",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 1, Byte: 0},
+							End:      hcl.Pos{Line: 1, Column: 12, Byte: 11},
+						},
+					},
+				},
+			},
+		},
+		{
+			"unknown block",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`bar {}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Unexpected block",
+						Detail:   "Blocks of type \"bar\" are not expected here",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 1, Byte: 0},
+							End:      hcl.Pos{Line: 1, Column: 4, Byte: 3},
+						},
+					},
+				},
+			},
+		},
+		{
+			"deprecated block",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						IsDeprecated: true,
+						Description: lang.MarkupContent{
+							Value: "Use `wakka` instead",
+							Kind:  lang.MarkdownKind,
+						},
+						Body: &schema.BodySchema{
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+	test =1
+}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagWarning,
+						Summary:  "\"foo\" is deprecated",
+						Detail:   "Reason: \"Use `wakka` instead\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 1, Byte: 0},
+							End:      hcl.Pos{Line: 1, Column: 4, Byte: 3},
+						},
+					},
+				},
+			},
+		},
+		{
+			"extra block labels",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Labels: []*schema.LabelSchema{
+							{
+								Name: "expected",
+							},
+						},
+						Body: &schema.BodySchema{
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo "expected" "notExpected" {
+	test = 1
+}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too many labels specified for \"foo\"",
+						Detail:   "Only 1 label(s) are expected for \"foo\" blocks",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 16, Byte: 15},
+							End:      hcl.Pos{Line: 1, Column: 29, Byte: 28},
+						},
+					},
+				},
+			},
+		},
+		{
+			"too few block labels",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Labels: []*schema.LabelSchema{
+							{
+								Name: "expected",
+							},
+							{
+								Name: "expected2",
+							},
+						},
+						Body: &schema.BodySchema{
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo "expected" {
+	test = 1
+}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Not enough labels specified for \"foo\"",
+						Detail:   "All \"foo\" blocks must have 2 label(s)",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 1, Byte: 0},
+							End:      hcl.Pos{Line: 1, Column: 4, Byte: 3},
+						},
+					},
+				},
+			},
+		},
+		{
+			"too many blocks",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"bar": {
+									MaxItems: 1,
+								},
+								"two": {},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				bar {}
+				bar {}
+				two {}
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too many blocks specified for \"bar\"",
+						Detail:   "Only 1 block(s) are expected for \"bar\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 5, Byte: 10},
+							End:      hcl.Pos{Line: 2, Column: 8, Byte: 13},
+						},
+					},
+				},
+			},
+		},
+		// either min or max is in schema but no blocks specified
+		{
+			"too few blocks",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MinItems: 2,
+								},
+								"two": {},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				one {}
+				two {}
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too few blocks specified for \"one\"",
+						Detail:   "At least 2 block(s) are expected for \"one\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 5, Byte: 10},
+							End:      hcl.Pos{Line: 2, Column: 8, Byte: 13},
+						},
+					},
+				},
+			},
+		},
+		{
+			"minitems with no blocks",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MinItems: 2,
+								},
+								"two": {},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too few blocks specified for \"one\"",
+						Detail:   "At least 2 block(s) are expected for \"one\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 1, Column: 5, Byte: 4},
+							End:      hcl.Pos{Line: 3, Column: 5, Byte: 23},
+						},
+					},
+				},
+			},
+		},
+		{
+			"min and max items with enough blocks for minitems",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MinItems: 2,
+									MaxItems: 4,
+								},
+								"two": {},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				one {}
+				one {}
+				one {}
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {},
+			},
+		},
+		{
+			"min and max set on two different blocks with correct number",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MinItems: 2,
+								},
+								"two": {
+									MaxItems: 1,
+								},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				one {}
+				one {}
+				two {}
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {},
+			},
+		},
+		{
+			"min and max set on two different blocks with incorrect number",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MinItems: 2,
+								},
+								"two": {
+									MaxItems: 1,
+								},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				one {}
+				two {}
+				two {}
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too few blocks specified for \"one\"",
+						Detail:   "At least 2 block(s) are expected for \"one\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 5, Byte: 10},
+							End:      hcl.Pos{Line: 2, Column: 8, Byte: 13},
+						},
+					},
+					&hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Too many blocks specified for \"two\"",
+						Detail:   "Only 1 block(s) are expected for \"two\"",
+						Subject: &hcl.Range{
+							Filename: "test.tf",
+							Start:    hcl.Pos{Line: 2, Column: 5, Byte: 10},
+							End:      hcl.Pos{Line: 2, Column: 8, Byte: 13},
+						},
+					},
+				},
+			},
+		},
+		{
+			"max is in schema but no blocks specified",
+			&schema.BodySchema{
+				Blocks: map[string]*schema.BlockSchema{
+					"foo": {
+						Body: &schema.BodySchema{
+							Blocks: map[string]*schema.BlockSchema{
+								"one": {
+									MaxItems: 4,
+								},
+								"two": {},
+							},
+							Attributes: map[string]*schema.AttributeSchema{
+								"test": {
+									Constraint: schema.LiteralType{Type: cty.Number},
+									IsRequired: true,
+								},
+							},
+						},
+					},
+				},
+			},
+			`foo {
+				test = 1
+			}`,
+			map[string]hcl.Diagnostics{
+				"test.tf": {},
+			},
 		},
 	}
 
@@ -59,7 +624,13 @@ func TestValidate_schema(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			if diff := cmp.Diff(tc.expectedDiagnostics, diags); diff != "" {
+			sortedDiags := diags["test.tf"]
+			sort.Slice(sortedDiags, func(i, j int) bool {
+				return sortedDiags[i].Subject.Start.Byte < sortedDiags[j].Subject.Start.Byte ||
+					sortedDiags[i].Summary < sortedDiags[j].Summary
+			})
+
+			if diff := cmp.Diff(tc.expectedDiagnostics["test.tf"], sortedDiags); diff != "" {
 				t.Fatalf("unexpected diagnostics: %s", diff)
 			}
 		})
