6f667b16ce5fe7a0c999082d55c6e88f62c63e83
Radek Simko <radek.simko@gmail.com>
2023-10-05 10:35:12 +0100

    schema: Introduce DefaultValue for AttributeSchema

---
+++ schema/default_value.go
@@ -0,0 +1,25 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import "github.com/zclconf/go-cty/cty"
+
+type defaultSigil struct{}
+
+type Default interface {
+	isDefaultImpl() defaultSigil
+}
+
+type DefaultValue struct {
+	Value cty.Value
+}
+
+func (dv DefaultValue) isDefaultImpl() defaultSigil {
+	return defaultSigil{}
+}
+
+// TODO: DefaultKeyword
+// TODO: DefaultTypeDeclaration
+// TODO: defaults dependent on other attributes
+// TODO: defaults dependent on env variables
