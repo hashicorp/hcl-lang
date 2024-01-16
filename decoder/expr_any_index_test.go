74b6b0feb10f5d31e82781f633b3da1841705d31
Daniel Banck <daniel@dbanck.de>
2024-01-16 17:09:32 +0100

    decoder: Test hover for complex index expr

--- decoder/expr_any_index_test.go
+++ decoder/expr_any_index_test.go
@@ -156,15 +156,46 @@ func TestCompletionAtPos_exprAny_index(t *testing.T) {
 					Addr: lang.Address{
 						lang.RootStep{Name: "aws_instance"},
 						lang.AttrStep{Name: "name"},
-						lang.AttrStep{Name: "tags"},
-						lang.IndexStep{Key: cty.StringVal("name")},
 					},
 					RangePtr: &hcl.Range{
 						Filename: "variables.tf",
 						Start:    hcl.Pos{Line: 2, Column: 1, Byte: 17},
 						End:      hcl.Pos{Line: 2, Column: 3, Byte: 19},
 					},
-					Type: cty.String,
+					Type: cty.ObjectWithOptionalAttrs(map[string]cty.Type{
+						"tags": cty.List(cty.String),
+					}, []string{"tags"}),
+					NestedTargets: reference.Targets{
+						{
+							Addr: lang.Address{
+								lang.RootStep{Name: "aws_instance"},
+								lang.AttrStep{Name: "name"},
+								lang.AttrStep{Name: "tags"},
+							},
+							RangePtr: &hcl.Range{
+								Filename: "variables.tf",
+								Start:    hcl.Pos{Line: 2, Column: 1, Byte: 17},
+								End:      hcl.Pos{Line: 2, Column: 3, Byte: 19},
+							},
+							Type: cty.List(cty.String),
+							NestedTargets: reference.Targets{
+								{
+									Addr: lang.Address{
+										lang.RootStep{Name: "aws_instance"},
+										lang.AttrStep{Name: "name"},
+										lang.AttrStep{Name: "tags"},
+										lang.IndexStep{Key: cty.StringVal("name")},
+									},
+									RangePtr: &hcl.Range{
+										Filename: "variables.tf",
+										Start:    hcl.Pos{Line: 2, Column: 1, Byte: 17},
+										End:      hcl.Pos{Line: 2, Column: 3, Byte: 19},
+									},
+									Type: cty.String,
+								},
+							},
+						},
+					},
 				},
 				{
 					Addr: lang.Address{
@@ -225,6 +256,116 @@ func TestCompletionAtPos_exprAny_index(t *testing.T) {
 
 			if diff := cmp.Diff(tc.expectedCandidates, candidates); diff != "" {
 				t.Fatalf("unexpected candidates: %s", diff)
+			}
+		})
+	}
+}
+
+func TestHoverAtPos_exprAny_index(t *testing.T) {
+	testCases := []struct {
+		testName          string
+		attrSchema        map[string]*schema.AttributeSchema
+		refOrigins        reference.Origins
+		refTargets        reference.Targets
+		cfg               string
+		pos               hcl.Pos
+		expectedHoverData *lang.HoverData
+	}{
+		{
+			"empty index",
+			map[string]*schema.AttributeSchema{
+				"attr": {
+					Constraint: schema.AnyExpression{
+						OfType: cty.String,
+					},
+				},
+			},
+			reference.Origins{},
+			reference.Targets{},
+			`attr = aws_instance.name.tags[]
+`,
+			hcl.Pos{Line: 1, Column: 31, Byte: 30},
+			nil,
+		},
+		{
+			"local reference in index",
+			map[string]*schema.AttributeSchema{
+				"attr": {
+					Constraint: schema.AnyExpression{
+						OfType: cty.String,
+					},
+				},
+			},
+			reference.Origins{
+				reference.LocalOrigin{
+					Addr: lang.Address{
+						lang.RootStep{Name: "local"},
+						lang.AttrStep{Name: "name"},
+					},
+					Range: hcl.Range{
+						Filename: "test.tf",
+						Start:    hcl.Pos{Line: 1, Column: 31, Byte: 30},
+						End:      hcl.Pos{Line: 1, Column: 41, Byte: 40},
+					},
+					Constraints: reference.OriginConstraints{
+						{
+							OfType: cty.String,
+						},
+					},
+				},
+			},
+			reference.Targets{
+				{
+					Addr: lang.Address{
+						lang.RootStep{Name: "local"},
+						lang.AttrStep{Name: "name"},
+					},
+					RangePtr: &hcl.Range{
+						Filename: "variables.tf",
+						Start:    hcl.Pos{Line: 2, Column: 1, Byte: 17},
+						End:      hcl.Pos{Line: 2, Column: 3, Byte: 19},
+					},
+					Type: cty.String,
+				},
+			},
+			`attr = aws_instance.name.tags[local.name]
+`,
+			hcl.Pos{Line: 1, Column: 35, Byte: 34},
+			&lang.HoverData{
+				Content: lang.Markdown("`local.name`\n_string_"),
+				Range: hcl.Range{
+					Filename: "test.tf",
+					Start:    hcl.Pos{Line: 1, Column: 31, Byte: 30},
+					End:      hcl.Pos{Line: 1, Column: 41, Byte: 40},
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d-%s", i, tc.testName), func(t *testing.T) {
+			bodySchema := &schema.BodySchema{
+				Attributes: tc.attrSchema,
+			}
+
+			f, _ := hclsyntax.ParseConfig([]byte(tc.cfg), "test.tf", hcl.InitialPos)
+			d := testPathDecoder(t, &PathContext{
+				Schema: bodySchema,
+				Files: map[string]*hcl.File{
+					"test.tf": f,
+				},
+				ReferenceOrigins: tc.refOrigins,
+				ReferenceTargets: tc.refTargets,
+			})
+
+			ctx := context.Background()
+			hoverData, err := d.HoverAtPos(ctx, "test.tf", tc.pos)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tc.expectedHoverData, hoverData); diff != "" {
+				t.Fatalf("unexpected hover data: %s", diff)
 			}
 		})
 	}
