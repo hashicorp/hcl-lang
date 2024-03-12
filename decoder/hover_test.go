67ff22b74b6f2848e9e1e698e5f9c963f1eb3b2b
Ansgar Mertens <ansgar@hashicorp.com>
2024-03-12 11:40:29 +0100

    test: add test case for hover for panics for invalid namespaced function expressions

--- decoder/hover_test.go
+++ decoder/hover_test.go
@@ -661,6 +661,34 @@ func TestDecoder_HoverAtPos_basic(t *testing.T) {
 	}
 }
 
+func TestDecoder_HoverAtPos_nil_expr(t *testing.T) {
+	// provider:: is not a traversal expression, so hcl will return a ExprSyntaxError which needs to be handled
+	f, _ := hclsyntax.ParseConfig([]byte(`attr = provider::`), "test.tf", hcl.InitialPos)
+
+	d := testPathDecoder(t, &PathContext{
+		Schema: &schema.BodySchema{
+			Attributes: map[string]*schema.AttributeSchema{
+				"attr": {Constraint: schema.AnyExpression{OfType: cty.DynamicPseudoType}},
+			},
+		},
+		Files: map[string]*hcl.File{
+			"test.tf": f,
+		},
+	})
+
+	ctx := context.Background()
+	_, err := d.HoverAtPos(ctx, "test.tf", hcl.Pos{Line: 1, Column: 16, Byte: 15})
+
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	positionalErr := &PositionalError{}
+	if !errors.As(err, &positionalErr) {
+		t.Fatal("expected PositionalError for invalid expression")
+	}
+}
+
 func TestDecoder_HoverAtPos_URL(t *testing.T) {
 	resourceLabelSchema := []*schema.LabelSchema{
 		{Name: "type", IsDepKey: true},
